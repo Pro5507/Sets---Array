da52d1e9603a8384bb1ddb267e559289c06f3e04
Pro5507 <nhupendrakumar9162@gmail.com>
2024-10-24 18:22:36 +0530

    Set & Array

---
+++ a.py
@@ -0,0 +1,5 @@
+import array as arr
+num= arr.array('i', [1,3,4,8,6])
+print(str(num))
+num.reverse()
+print(str(num))
---
+++ set.py
@@ -0,0 +1,18 @@
+set= {1,2,3,4,5}
+print(set)
+a= {1,2,3,3,4,4,5,5,5}
+print(a)
+a.pop()
+print(a)
+x= {"Green", "Pink"}
+y={"Green", "Red"}
+print(x)
+print(y)
+z=x.intersection(y)
+print(z)
+p=x.union(y)
+print(p)
+d= x.difference(y)
+print(d)
+e= x.symmetric_difference(y)
+print(e)
